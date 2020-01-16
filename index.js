68d94518da409c7e9e310bad6d6549bca984932f
pb-totoro <jonesec2@gmail.com>
2020-01-15 20:53:33 -0500

    Added another axios call with user info

--- index.js
+++ index.js
@@ -5,31 +5,31 @@ const inquirer = require("inquirer");
 inquirer
     .prompt({
         message: "Enter your GitHub username",
-        name: "username"
+        name: "username",
     })
     // the function is using objDesctructoring as a parameter to only get back the "username" from the api request
     .then(function ({ username }) {
         const repoQuery = `https://api.github.com/users/${username}/repos?per_page=100`;
-        const userQuery = `https://api.github.com/users/jonesec2`
+        const userQuery = `https://api.github.com/users/${username}`;
+        const followerQuery = `https://api.github.com/users/${username}/followers`;
 
         axios.get(repoQuery).then(function (res) {
-            // console.log(res.data[0])
 
-            const stars = res.data.map(repo => repo.stargazers_count)
-            // console.log(stars)
+            // gets the number of stars for each repo
+            const stars = res.data.map(repo => repo.stargazers_count);
+            // condenses the array into a single total of stars
             const starsTotal = stars.reduce( (total, num) => total + num);
-            console.log(starsTotal);
-
+            // gets the names of all the repo's
             const repoNames = res.data.map( repo => repo.name);
-            console.log(repoNames)
-
+            // joins all of the repos and creates a new line for each new repo
             const repoNamesStr = repoNames.join("\n");
 
+            // writes the info to text file
             fs.writeFile("repos.txt", repoNamesStr, function (err) {
                 if (err) {
                     throw err;
                 }
-
+                //message showing # of repos
                 console.log(`saved ${repoNames.length} repos`)
 
             })
@@ -37,10 +37,21 @@ inquirer
 
         axios.get(userQuery).then(function (res) {
 
-            const location = res.data.location
-            console.log(location)
+            const location = res.data.location;
+            console.log(location);
 
-            const picture = res.data.owner.avatar_url
-            console.log(picture)
+            const picture = res.data.avatar_url;
+            console.log(picture);
+
+            const bio = res.data.bio;
+
+            // const followerCount = res.data.
         })
     })
+    // prompt({
+    //     message: "Pick your favorite color",
+    //     chosenColor:  "color"
+    // })
+    // .then(function ({ color }) {
+    //     const useColor = color
+    // })
