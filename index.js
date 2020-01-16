03780be8588484277cda428e9eb97a8ffc72feaf
pb-totoro <jonesec2@gmail.com>
2020-01-15 21:27:55 -0500

    Refactored axios into two calls

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ inquirer
         message: "Enter your GitHub username",
         name: "username",
     })
-    // the function is using objDesctructoring as a parameter to only get back the "username" from the api request
+    
     .then(function ({ username }) {
         const repoQuery = `https://api.github.com/users/${username}/repos?per_page=100`;
         const userQuery = `https://api.github.com/users/${username}`;
-        const followerQuery = `https://api.github.com/users/${username}/followers`;
 
         axios.get(repoQuery).then(function (res) {
 
             // gets the number of stars for each repo
-            const stars = res.data.map(repo => repo.stargazers_count);
             // condenses the array into a single total of stars
+            const stars = res.data.map(repo => repo.stargazers_count);
             const starsTotal = stars.reduce( (total, num) => total + num);
+
             // gets the names of all the repo's
             const repoNames = res.data.map( repo => repo.name);
+
             // joins all of the repos and creates a new line for each new repo
             const repoNamesStr = repoNames.join("\n");
 
@@ -30,22 +31,22 @@ inquirer
                     throw err;
                 }
                 //message showing # of repos
-                console.log(`saved ${repoNames.length} repos`)
-
+                console.log(`saved ${repoNames.length} repos`);
             })
         });
 
         axios.get(userQuery).then(function (res) {
 
+            const repoNumber = res.data.public_repos;
             const location = res.data.location;
-            console.log(location);
-
             const picture = res.data.avatar_url;
-            console.log(picture);
-
             const bio = res.data.bio;
+            const name = res.data.name;
+            const gitHub = res.data.html_url;
+            const blog = res.data.blog;
+            const followers = res.data.followers;
+            const following = res.data.following;
 
-            // const followerCount = res.data.
         })
     })
     // prompt({
