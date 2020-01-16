8f9599cd29fcb3d1fdcbd13a61144c3df7981202
pb-totoro <jonesec2@gmail.com>
2020-01-15 20:42:14 -0500

    Added another axios for user

--- index.js
+++ index.js
@@ -9,22 +9,19 @@ inquirer
     })
     // the function is using objDesctructoring as a parameter to only get back the "username" from the api request
     .then(function ({ username }) {
-        const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-        `https://api.github.com/users/jonesec2`
+        const repoQuery = `https://api.github.com/users/${username}/repos?per_page=100`;
+        const userQuery = `https://api.github.com/users/jonesec2`
 
-        axios.get(queryUrl).then(function (res) {
-            // console.log(res)
+        axios.get(repoQuery).then(function (res) {
+            // console.log(res.data[0])
 
-            const stars = res.data.map(function (repo) {
-                return repo.stargazers_count
-            })
-            console.log(stars)
-            const starsTotal = stars.reduce((total, num) => total + num);
+            const stars = res.data.map(repo => repo.stargazers_count)
+            // console.log(stars)
+            const starsTotal = stars.reduce( (total, num) => total + num);
             console.log(starsTotal);
 
-            const repoNames = res.data.map(function (repo) {
-                return repo.name;
-            });
+            const repoNames = res.data.map( repo => repo.name);
+            console.log(repoNames)
 
             const repoNamesStr = repoNames.join("\n");
 
@@ -37,4 +34,13 @@ inquirer
 
             })
         });
+
+        axios.get(userQuery).then(function (res) {
+
+            const location = res.data.location
+            console.log(location)
+
+            const picture = res.data.owner.avatar_url
+            console.log(picture)
+        })
     })
